321b670a04c6f599c1eeec6bb33de39e069fe143
D4rk4ns <jonathanlsr9611@gmail.com>
2022-02-22 18:07:44 -0500

    test2

--- config/database.js
+++ config/database.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose'),
+const mongoose = require('mongoose');
     
     require('dotenv').config();
 
     const uri = process.env.ATLAS_URI;
     mongoose.connect(uri);
     mongoose.connection.once('open', () => {
-        
-        
         console.log('Database: \x1b[32m%s\x1b[0m', 'online');
-
     });
+
 module.exports = mongoose;
 
