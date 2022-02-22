fbf152bcc6b9386682bc9ec7c6804de086f34020
D4rk4ns <jonathanlsr9611@gmail.com>
2022-02-22 17:55:11 -0500

    ASDSAd

--- config/database.js
+++ config/database.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose'),
     require('dotenv').config();
 
     mongoose.connection.openUri(process.env.ATLAS_URI , (err, res) => {
-        if (err.code === 'ERR_UNHANDLED_REJECTION') throw 'Check your AtlasDB network configuration';
+        if (err.code.equals('ERR_UNHANDLED_REJECTION')) throw 'Check your AtlasDB network configuration';
+        
         if (err) throw err.message;
-
+        
         console.log('Database: \x1b[32m%s\x1b[0m', 'online');
 
     });
