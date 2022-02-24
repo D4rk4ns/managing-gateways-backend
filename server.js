fbd2ced0ab81ae5b552572601a7fddd9c8692f9a
Jonathan Luis Rodriguez <80186443+D4rk4ns@users.noreply.github.com>
2022-02-23 23:51:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ app.use(helmet());
 
 //Parse application/json and look for raw text                                        
 app.use(bodyParser.json());                                     
-app.use(bodyParser.urlencoded({extended: false}));               
-app.use(bodyParser.text());                                    
 app.use(bodyParser.json({ type: 'application/json'}));  
 
 // Enabling CORS for all requests
