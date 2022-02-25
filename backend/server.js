a720cb98d3c633e7730caaae5c5c385bb9f94583
Jonathan Luis Rodriguez <80186443+D4rk4ns@users.noreply.github.com>
2022-02-25 14:42:35 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -64,12 +64,11 @@ const favicon = require("express-favicon");
 const path    = require("path");
 
 // __dirname is the current directory from where the script is running
+app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(express.static(__dirname));
-console.log(favicon("../public/favicon.ico"));
-app.use(favicon("../public/favicon.ico"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/*', function (req, res){
+app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 /****	Heroku purposes only 	***/
